3ac8e9ed6d42329a2c424678225e805f3c8af513
unknown <sayurabh@gmail.com>
2019-05-09 07:57:12 -0700

    bevt

--- myproject/views.py
+++ myproject/views.py
@@ -2,7 +2,7 @@ from django.template import Context, RequestContext
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
-import urllib2
+import urllib
 from django.http import JsonResponse
 # your other views
 #
@@ -22,6 +22,6 @@ def callback1(request):
 	number = request.GET.get('number')
 	print number
 	params = {'apikey': '7caYobsaaiU-MRLoIoWisTON1aM7KUeTVcDgwA1Hs', 'numbers':'9711143354', 'message' :'message', 'sender': 'DLPHRM'}
-	f = urllib2.urlopen('https://api.textlocal.in/send/?'+ urllib.parse.urlencode(params))
+	f = urllib.request.urlopen('https://api.textlocal.in/send/?'+ urllib.parse.urlencode(params))
 	return (f.read(), f.code)
 	
