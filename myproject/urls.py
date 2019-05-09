6b2c443eb9b0cb330691b062edc33f8e98572492
unknown <sayurabh@gmail.com>
2019-05-08 18:25:01 -0700

    erg

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from views import callback
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
      url(r'^callback$', view=callback, name='callback')
