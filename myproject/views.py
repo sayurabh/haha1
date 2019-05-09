99967b59a959b5bf9952941ec2814ffcf999d851
unknown <sayurabh@gmail.com>
2019-05-09 08:10:16 -0700

    bevt

--- myproject/views.py
+++ myproject/views.py
@@ -3,6 +3,7 @@ from django.template import Context, RequestContext
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 import urllib.request
+import urllib.parse
 from django.http import JsonResponse
 # your other views
 #
