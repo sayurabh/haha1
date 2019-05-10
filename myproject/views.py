0b257cd31a61e98c13d1d9184ae571bc261d32dc
unknown <sayurabh@gmail.com>
2019-05-09 18:28:50 -0700

    bevt

--- myproject/views.py
+++ myproject/views.py
@@ -27,9 +27,9 @@ def callback1(request):
 	otp = str(randint(1000, 9999))
 	#params = {'apikey': '7caYobsaaiU-MRLoIoWisTON1aM7KUeTVcDgwA1Hs', 'numbers':'9711143354', 'message' :'message', 'sender': 'DLPHRM'}
 	#data = urllib.urlencode(params)
-	#req = urllib2.Request('https://api.textlocal.in/send/?apikey=7caYobsaaiU-MRLoIoWisTON1aM7KUeTVcDgwA1Hsi&sender=DLPHRM&numbers='+number+'&message=Your%20OTP%20is%20'+otp)
-	#f = urllib2.urlopen(req)
-	#the_page = f.read()
-	#print the_page
+	req = urllib2.Request('https://api.textlocal.in/send/?apikey=7caYobsaaiU-MRLoIoWisTON1aM7KUeTVcDgwA1Hsi&sender=DLPHRM&numbers='+number+'&message=Your%20OTP%20is%20'+otp)
+	f = urllib2.urlopen(req)
+	the_page = f.read()
+	print the_page
 	return HttpResponse("return this string")
 	
