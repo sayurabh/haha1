4c0d57f9377249c79cdafd720ae6c2dd19ef8790
unknown <sayurabh@gmail.com>
2019-05-10 16:28:53 -0700

    fewr

--- myproject/views.py
+++ myproject/views.py
@@ -34,5 +34,6 @@ def callback1(request):
 	the_page = f.read()
 	print the_page
 	#posts_serialized = serializers.serialize('json', the_page)
-	return JsonResponse(json.dumps(the_page),safe=False ) 
+	#return JsonResponse(json.dumps(the_page),safe=False ) 
+	HttpResponse(the_page)
 	
