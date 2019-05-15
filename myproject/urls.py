72eba1d874123b260711b65580b310d6950f4f55
unknown <sayurabh@gmail.com>
2019-05-15 09:48:03 -0700

    sf

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from views import callback,callback1
+from views import callback,callback1,fetchhaha
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
      url(r'^callback$', view=callback, name='callback'),
-       url(r'^callback1$', view=callback1, name='callback1')
+       url(r'^callback1$', view=callback1, name='callback1'),
+        url(r'^fetch$', view=fetchhaha, name='fetch')
 ]
--- myproject/views.py
+++ myproject/views.py
@@ -65,5 +65,20 @@ def callback1(request):
 	print the_page
 
 	return JsonResponse(json.dumps(the_page),safe=False ) 
-	#return HttpResponse("pop")
+def fetchhaha(request):
+	conn = psycopg2.connect(database = "myproject", user = "myprojectuser", password = "password", host = "127.0.0.1")
+	print "Opened database successfully"
+	cursor = connection.cursor()
+	postgreSQL_select_Query = "select * from company"
+	cursor.execute(postgreSQL_select_Query)
+	print("Selecting rows from mobile table using cursor.fetchall")
+	mobile_records = cursor.fetchall() 
+	print("Print each row and it's columns values")
+	for row in mobile_records:
+       print("Id = ", row[0], )
+       print("Model = ", row[1])
+       print("Price  = ", row[2])
+       print("sdfg = ",row[3], "\n")
+    conn.close()
+	return HttpResponse("pop")
 	
