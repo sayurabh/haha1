1c67e64a148994235064400aeaa01237427c4d38
unknown <sayurabh@gmail.com>
2019-06-16 00:46:10 -0700

    Vetrhg

--- myproject/views.py
+++ myproject/views.py
@@ -170,6 +170,7 @@ def fetchhah(request):
 	cursor.execute(postgreSQL_select_Query)
 	print("Selecting rows from mobile table using cursor.fetchall")
 	mobile_records = cursor.fetchall() 
+	print len(mobile_records)
 	print("Print each row and it's columns values")
 	for row in mobile_records:
 		print("Id = ", row[0], )
