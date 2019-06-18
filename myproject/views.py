49fba40effda7f16e2cba6026b039bacda7f5236
unknown <sayurabh@gmail.com>
2019-06-17 18:03:06 -0700

    dersg

--- myproject/views.py
+++ myproject/views.py
@@ -139,6 +139,7 @@ def callback3(request):
 	mobile = json.dumps(cur.fetchall())
 	json_1 = json.loads(mobile)
 	json_13 = json_1[0:9]
+	print json_13
 	json_12 = json_13.append({"nump":"9"})
 	print json_12
 	#json_2 = json.dumps(json_1[0:9])
